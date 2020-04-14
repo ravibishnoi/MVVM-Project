04e302dc275dfc1f36d8cf653ec5847c4763677d
ravibishnoi <rbk4997@gmail.com>
2020-04-14 19:47:48 +0530

    the first MVVM Project

---
+++ MvvmProject/Model/MovieModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieModel.swift
+//  MvvmProject
+//
+//  Created by AshutoshD on 10/04/20.
+//  Copyright © 2020 ravindraB. All rights reserved.
+//
+
+import UIKit
+
+class MovieModel: NSObject {
+
+}
---
+++ MvvmProject/View Model/MovieViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieViewModel.swift
+//  MvvmProject
+//
+//  Created by AshutoshD on 10/04/20.
+//  Copyright © 2020 ravindraB. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewModel: NSObject {
+
+}
